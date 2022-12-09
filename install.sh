b8ef4fbb1eebbae228f55fb4578979515bc241b8
Adrian Barbos <adrianb@ensemble.com>
2022-12-09 15:22:09 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo
+echo "###############################"
+echo "#  Cloudmanger setup started  #"
+echo "###############################"
+echo
+
+echo "[1/X] Cloning cloud manager microservices"
+echo
+git clone git@github.com:radiantlogic-v8/cloudmanager-ui.git
+git clone git@github.com:radiantlogic-v8/cloudmanager-backend.git
+git clone git@github.com:radiantlogic-v8/cloudmanager-environment-orchestrator.git
+
+echo
+echo "###############################"
+echo "#   Cloudmanger setup done    #"
+echo "###############################"
+echo
