aeb09e3511c6285fb760302e4c0f167cafbea35f
Adrian Barbos <adrianb@ENSRO0235.local>
2022-12-15 16:31:14 +0200

    Partial commit adding helm

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ echo
 
 echo "[1/X] Cloning cloud manager microservices"
 echo
+mkdir src
+cd src/
+
 git clone git@github.com:radiantlogic-v8/cloudmanager-ui.git
 git clone git@github.com:radiantlogic-v8/cloudmanager-backend.git
 git clone git@github.com:radiantlogic-v8/cloudmanager-environment-orchestrator.git
